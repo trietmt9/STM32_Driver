0172cfa1581fe78b8803bef9972bfcabdf899137
Triet <76050658+trietmt9@users.noreply.github.com>
2023-12-07 08:19:53 +0700

    update driver

--- STM32F446RE_DRIVER/Drivers/Inc/stm32f446xx.h
+++ STM32F446RE_DRIVER/Drivers/Inc/stm32f446xx.h
@@ -37,7 +37,7 @@
 #define GPIOF_BASE_ADDRESS              (AHB1_PERIPHERAL_BASE + 0x1400UL)
 #define GPIOG_BASE_ADDRESS              (AHB1_PERIPHERAL_BASE + 0x1800UL)
 #define GPIOH_BASE_ADDRESS              (AHB1_PERIPHERAL_BASE + 0x1C00UL)
-
+#define RCC_BASE_ADDRESS                (AHB1_PERIPHERAL_BASE + 0x3800UL)
 /* 
  * Define base address peripherals connected to APB1 bus  
  */
@@ -45,8 +45,6 @@
 #define I2C1_BASE_ADDRESS               (APB1_PERIPHERAL_BASE + 0x5400UL)
 #define I2C2_BASE_ADDRESS               (APB1_PERIPHERAL_BASE + 0x5800UL)
 #define I2C3_BASE_ADDRESS               (APB1_PERIPHERAL_BASE + 0x5C00UL)
-#define USART2_BASE_ADDRESS             (APB1_PERIPHERAL_BASE + 0x4400UL)
-#define USART3_BASE_ADDRESS             (APB1_PERIPHERAL_BASE + 0x4800UL)
 #define USART2_BASE_ADDRESS             (APB1_PERIPHERAL_BASE + 0x4400UL)
 #define USART3_BASE_ADDRESS             (APB1_PERIPHERAL_BASE + 0x4800UL)
 #define UART4_BASE_ADDRESS              (APB1_PERIPHERAL_BASE + 0x4C00UL)
@@ -60,6 +58,7 @@
 
 #define EXTI_BASE_ADDRESS               (APB2_PERIPHERAL_BASE + 0x3C00UL)
 #define SPI1_BASE_ADDRESS               (APB2_PERIPHERAL_BASE + 0x3000UL)
+#define SPI4_BASE_ADDRESS               (APB2_PERIPHERAL_BASE + 0x3400UL)
 #define USART1_BASE_ADDRESS             (APB2_PERIPHERAL_BASE + 0x1000UL)
 #define USART6_BASE_ADDRESS             (APB2_PERIPHERAL_BASE + 0x1400UL)
 #define SYSCFG_BASE_ADDRESS             (APB2_PERIPHERAL_BASE + 0x3800UL)
@@ -67,6 +66,7 @@
 
 /* 
  * Define peripheral register structure for GPIO  
+ * Read GPIO register map and reset values in the datasheet 
  */
 
 typedef struct 
@@ -85,6 +85,52 @@ typedef struct
 }GPIO_RegDef_t;
 
 /* 
+ * Define peripheral register structure for RCC
+ * Read RCC register map and reset values in the datasheet  
+ */
+typedef struct 
+{
+    volatile uint32_t CR;
+    volatile uint32_t PLLCFGR;
+    volatile uint32_t CIR;
+    volatile uint32_t AHB1RSTR;
+    volatile uint32_t AHB2RSTR;
+    volatile uint32_t AHB3RSTR;
+    uint32_t Reserved0; 
+    volatile uint32_t APB1RSTR;  
+    volatile uint32_t APB2RSTR; 
+    uint32_t Reserved1; 
+    uint32_t Reserved2;
+    volatile uint32_t AHB1ENR;
+    volatile uint32_t AHB2ENR;
+    volatile uint32_t AHB3ENR;
+    uint32_t Reserved3; 
+    volatile uint32_t APB1ENR;
+    volatile uint32_t AHB2ENR;
+    uint32_t Reserved4; 
+    uint32_t Reserved5;
+    volatile uint32_t AHB1LPENR;
+    volatile uint32_t AHB2LPENR;
+    volatile uint32_t AHB3LPENR;
+    uint32_t Reserved6;
+    volatile uint32_t APB1LPENR;
+    volatile uint32_t APB2LPENR;
+    uint32_t Reserved7; 
+    uint32_t Reserved8;
+    volatile uint32_t BDCR;
+    volatile uint32_t CSR;
+    uint32_t Reserved9; 
+    uint32_t Reserved10;
+    volatile uint32_t SSCGR;
+    volatile uint32_t PLL2SCFGR;
+    volatile uint32_t PLLSAICFGR;
+    volatile uint32_t DCKCFGR;
+    volatile uint32_t CKGATENR;
+    volatile uint32_t DCKCFGR2;
+
+}RCC_RegDef_t;
+
+/* 
  * Peripheral definitions
  */
 
@@ -96,6 +142,51 @@ typedef struct
 #define GPIOF                           ((GPIO_RegDef_t*)GPIOF_BASE_ADDRESS)
 #define GPIOG                           ((GPIO_RegDef_t*)GPIOG_BASE_ADDRESS)
 #define GPIOH                           ((GPIO_RegDef_t*)GPIOH_BASE_ADDRESS)
+#define RCC                             ((RCC_RegDef_t*)RCC_BASE_ADDRESS)
 
+/* 
+ * CLock enable macros for GPIOx peripherial 
+ */
+
+#define GPIOA_PCLK_EN()                 (RCC->AHB1ENR |= (1<<0))
+#define GPIOB_PCLK_EN()                 (RCC->AHB1ENR |= (1<<1))
+#define GPIOC_PCLK_EN()                 (RCC->AHB1ENR |= (1<<2))
+#define GPIOD_PCLK_EN()                 (RCC->AHB1ENR |= (1<<3))
+#define GPIOE_PCLK_EN()                 (RCC->AHB1ENR |= (1<<4))
+#define GPIOF_PCLK_EN()                 (RCC->AHB1ENR |= (1<<5))
+#define GPIOG_PCLK_EN()                 (RCC->AHB1ENR |= (1<<6))
+#define GPIOH_PCLK_EN()                 (RCC->AHB1ENR |= (1<<7))
+
+/* 
+ * CLock enable macros for I2Cx peripherial 
+ */
+
+#define I2C1_PCLK_EN()                  (RCC->APB1ENR |= (1<<21))
+#define I2C2_PCLK_EN()                  (RCC->APB1ENR |= (1<<22))
+#define I2C3_PCLK_EN()                  (RCC->APB1ENR |= (1<<23))
+
+/* 
+ * CLock enable macros for SPIx peripherial 
+ */
+
+#define SPI1_PCLK_EN()                  (RCC->APB2ENR |= (1<<12))
+#define SPI2_PCLK_EN()                  (RCC->APB1ENR |= (1<<14))
+#define SPI3_PCLK_EN()                  (RCC->APB1ENR |= (1<<15))
+#define SPI4_PCLK_EN()                  (RCC->APB2ENR |= (1<<13))
+
+/* 
+ * CLock enable macros for USARTx peripherial 
+ */
+
+#define USART1_PCLK_EN()                (RCC->APB2ENR |= (1<<4))
+#define USART2_PCLK_EN()                (RCC->APB1ENR |= (1<<17))
+#define USART3_PCLK_EN()                (RCC->APB1ENR |= (1<<18))
+#define UART4_PCLK_EN()                 (RCC->APB2ENR |= (1<<19))
+#define UART5_PCLK_EN()                 (RCC->APB1ENR |= (1<<20))
+#define USART6_PCLK_EN()                (RCC->APB2ENR |= (1<<5))
+
+/* 
+ * CLock enable macros for SYSCFG peripherial 
+ */
 
 #endif
